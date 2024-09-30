a124f1da10876b87cf382f55b3c086fef5bf1165
jinniekim <jinhgkim@gmail.com>
2024-09-30 10:12:34 -0700

    Build and render blocks and apply rotations

--- pa1/block.cpp
+++ pa1/block.cpp
@@ -4,34 +4,86 @@
 
 int Block::width() const
 {
-/* YOUR CODE HERE */
+    return data[0].size();
 }
 int Block::height() const
 {
-/* YOUR CODE HERE */
+    return data.size();
 }
 
 void Block::render(PNG &im, int column, int row) const
 {
-/* YOUR CODE HERE */
+    for (int y = row; y < row + height(); y++)
+    {
+        for (int x = column; x < column + width(); x++)
+        {
+            RGBAPixel *imgPixel = im.getPixel(x, y);
+            RGBAPixel dataPixel = data[y - row][x - column];
+            imgPixel->r = dataPixel.r;
+            imgPixel->g = dataPixel.g;
+            imgPixel->b = dataPixel.b;
+            imgPixel->a = dataPixel.a;
+        }
+    }
 }
 
 void Block::build(PNG &im, int column, int row, int width, int height)
 {
-/* YOUR CODE HERE */
+    data.resize(height, vector<RGBAPixel>(width));
+
+    for (int y = row; y < row + height; y++)
+    {
+        for (int x = column; x < column + width; x++)
+        {
+            RGBAPixel *imgPixel = im.getPixel(x, y);
+            RGBAPixel *dataPixel = &data[y - row][x - column];
+            dataPixel->r = imgPixel->r;
+            dataPixel->g = imgPixel->g;
+            dataPixel->b = imgPixel->b;
+            dataPixel->a = imgPixel->a;
+        }
+    }
 }
 
 void Block::flipVert()
 {
-/* YOUR CODE HERE */
+    int height = this->height();
+    int width = this->width();
+    for (int y = 0; y < height / 2; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            RGBAPixel temp = data[y][x];
+            data[y][x] = data[height - 1 - y][x];
+            data[height - 1 - y][x] = temp;
+        }
+    }
 }
 
 void Block::flipHoriz()
 {
-/* YOUR CODE HERE */
+    int height = this->height();
+    int width = this->width();
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width / 2; x++)
+        {
+            RGBAPixel temp = data[y][x];
+            data[y][x] = data[y][width - 1 - x];
+            data[y][width - 1 - x] = temp;
+        }
+    }
 }
 
 void Block::rotateRight()
 {
-/* YOUR CODE HERE */
+    int height = this->height();
+    int width = this->width();
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            //
+        }
+    }
 }
